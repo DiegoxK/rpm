9a6fb52734616bbb58edf5b5118281ac409122bb
DiegoxK <diego.stv@outlook.es>
2024-04-25 17:04:17 -0500

    Delete unused assets

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,4 @@
 import logo from "@/public/RPMLogo.svg";
-import hero from "@/public/heroimg.svg";
-
-import WebDesign from "@/public/services/WebDesign.webp";
-import Strategies from "@/public/services/Strategies.webp";
-import Advertisement from "@/public/services/Advertisement.webp";
-
-import Local from "@/public/about/Local.svg";
-import Results from "@/public/about/Results.svg";
-import Specialized from "@/public/about/Specialized.svg";
 
 type NavigationItem = {
   name: string;
@@ -37,11 +28,4 @@ export const siteConfig: SiteConfig = {
 
 export const assets = {
   logo,
-  hero,
-  WebDesign,
-  Strategies,
-  Advertisement,
-  Local,
-  Results,
-  Specialized,
 };
